9220c278b74600c36cb17cca4a197a42fb944e23
Maxim Yefremov <maxYefr@gmail.com>
2018-04-26 09:53:41 +0300

    more tests

--- binary_numbers/main_test.go
+++ binary_numbers/main_test.go
@@ -10,11 +10,16 @@ func TestToBinaryAndOneCount(t *testing.T){
 		in, o int
 		b string
 	}
-	for _, tc := range []testCase{{13, 2,  "1101"}} {
-		r, o, err := toBinaryAndOneCount(tc.in)
-		as := assert.New(t)
-		as.NoError(err)
-		as.Equal(tc.b, r)
-		as.Equal(tc.o, o)
+	for _, tc := range []testCase{
+		{13, 2,  "1101"},
+		{5, 1,  "101"},
+	} {
+		t.Run("test"+tc.b, func (t *testing.T){ // https://youtu.be/hVFEV-ieeew?t=1037
+			r, o, err := toBinaryAndOneCount(tc.in)
+			as := assert.New(t)
+			as.NoError(err)
+			as.Equal(tc.b, r)
+			as.Equal(tc.o, o)
+		})
 	}
 }
