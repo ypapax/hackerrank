c93a61644307e84df37da81acc12d9f6474394b9
Maxim Yefremov <maxYefr@gmail.com>
2018-04-29 20:17:48 +0300

    drag drop colors

--- balls_containers/fe/src/Components/Cell.js
+++ balls_containers/fe/src/Components/Cell.js
@@ -1,6 +1,11 @@
 import React, {Component} from 'react';
 
 class Cell extends Component {
+    constructor() {
+        super();
+        this.state = {};
+    }
+
     allowDrop(e) {
         e.preventDefault();
     }
@@ -10,16 +15,27 @@ class Cell extends Component {
         var senderRow = ev.dataTransfer.getData("row");
         var senderCol = ev.dataTransfer.getData("column");
         this.props.onDrop(senderRow, senderCol, this.props.row, this.props.column);
+        this.state.dropping = true;
     }
 
     dragStart(ev) {
         ev.dataTransfer.setData("row", this.props.row);
         ev.dataTransfer.setData("column", this.props.column);
+        this.setState({
+            dragging: true
+        })
     }
 
     render() {
+        let dragDropStyle = "";
+        if (this.state.dragging) {
+            dragDropStyle = "drag";
+        } else if (this.state.dropping)  {
+            dragDropStyle = "drop";
+        }
+        let className = "cell " + dragDropStyle;
         return (
-            <td className="cell" draggable="true" onDrop={this.onDrop.bind(this)}
+            <td className={className} draggable="true" onDrop={this.onDrop.bind(this)}
                 onDragStart={this.dragStart.bind(this)}
                 onDragOver={this.allowDrop.bind(this)}
             >
