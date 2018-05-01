01aef574fbb5dc57de843b407e6810da413c2cdd
Maxim Yefremov <maxYefr@gmail.com>
2018-05-01 07:17:24 +0300

    java solution

---
+++ balls_containers/java/run.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -ex
+javac ./Solution.java
+# https://raw.githubusercontent.com/charles-wangkai/hackerrank/master/organizing-containers-of-balls/Solution.java
+cat 4_big.txt | java Solution
